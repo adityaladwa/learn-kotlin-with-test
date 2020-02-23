7efc7b27cf8539e3b6b22f5bd88f3da5cb23dd8f
Aditya Ladwa <ladwa.aditya@gmail.com>
2020-02-23 23:15:12 +0530

    Basic: Function with test

--- src/main/kotlin/Basic.kt
+++ src/main/kotlin/Basic.kt
@@ -1,3 +1,8 @@
 fun main() {
+    println(sum(2, 4))
+}
 
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
 }
--- src/test/kotlin/BasicTest.kt
+++ src/test/kotlin/BasicTest.kt
@@ -1,9 +1,12 @@
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class BasicTest {
 
     @Test
-    fun `should pass`() {
-
+    fun `Sum of a and b`() {
+        val expected = 4
+        val actual = sum(2, 2)
+        assertEquals(expected, actual)
     }
 }
